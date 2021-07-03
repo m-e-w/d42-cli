d17e0c33bb2d797466f71b274ab913166ac282d3
m-e-w <walls.e.matthew@gmail.com>
2021-07-03 18:30:21 -0400

    Updated list room

---
+++ lib/doql/list_room.sql
@@ -0,0 +1,31 @@
+WITH target_data AS (
+    SELECT
+        CONCAT(b.name, ' | ', ro.name) AS location,
+        b.name AS building_name,
+        CONCAT(
+            'Grid Size: ',
+            ro.grid_cols * ro.grid_rows,
+            ' (',
+            ro.grid_cols,
+            ' Columns, ',
+            ro.grid_rows,
+            ' Rows)'
+        ) AS grid,
+        ro.notes AS room_notes,
+        ro.tags AS room_tags,
+        CONCAT(
+            'https://$($d42_host)/admin/rackraj/room/',
+            ro.room_pk
+        ) AS url
+    FROM
+        view_room_v1 ro
+        JOIN view_building_v1 b ON b.building_pk = ro.building_fk
+)
+SELECT
+    td.location AS name,
+    td.grid,
+    td.room_notes AS notes,
+    td.room_tags as tags,
+    td.url
+FROM
+    target_data td
